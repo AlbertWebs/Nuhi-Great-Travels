33b403b8fd746132b8f27926988e5ad1403fc013
AlbertWebs <albertmuhatia@gmail.com>
2025-10-23 12:13:27 +0300

    Update manifest

--- resources/views/layouts/app-layout.blade.php
+++ resources/views/layouts/app-layout.blade.php
@@ -17,7 +17,7 @@
   <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-touch-icon.png')}}">
   <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">
   <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
-  <link rel="manifest" href="{{asset('favicon//site.webmanifest')}}">
+  <link rel="manifest" href="{{asset('favicon/site.webmanifest')}}">
 </head>
 <body class="font-sans antialiased bg-gray-50 text-gray-900 min-h-screen flex flex-col">
 <?php $Settings = \App\Models\Setting::first(); ?>
